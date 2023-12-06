d9b888db1640afa45f2ba495d5b0042627a6ea8c
MiPigurim <michelepigurim26@gmail.com>
2023-12-06 17:05:25 -0300

    feat:install and import bootstrap and font-awesome

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div></div>
+  <div class="text-center">
+    <h1>NameGator <span class="fa fa-check"></span></h1>
+  </div>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 export default {
   name: "App",
 };
