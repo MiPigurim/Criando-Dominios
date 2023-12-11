c761ec247a1ed6a879931cdbdb423faf4dedce41
MiPigurim <michelepigurim26@gmail.com>
2023-12-11 17:04:40 -0300

    feat: cleaning App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,58 +9,7 @@
     </div>
 
     <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <AppItemListVue
-              title="Prefixos"
-              v-bind:items="prefixes"
-              v-on:addItem="addPrefix"
-              v-on:deleteItem="deletePrefix"
-            ></AppItemListVue>
-          </div>
-          <div class="col-md">
-            <app-item-list-vue
-              title="Sufixo"
-              v-bind:items="sufixes"
-              v-on:addItem="addSufix"
-              v-on:deleteItem="deleteSufix"
-            ></app-item-list-vue>
-          </div>
-        </div>
-
-        <br />
-
-        <h5>
-          Domínios <span class="badge badge-info"> {{ domains.length }} </span>
-        </h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li
-                class="list-group-item"
-                v-for="domain in domains"
-                v-bind:key="domain.name"
-              >
-                <div class="row">
-                  <div class="col-md">
-                    {{ domain.name }}
-                  </div>
-                  <div class="col-md text-right">
-                    <a
-                      class="btn btn-info"
-                      v-bind:href="domain.checkout"
-                      target="_blank"
-                    >
-                      <span class="fa fa-shopping-cart"></span>
-                    </a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <DomainList></DomainList>
     </div>
   </div>
 </template>
@@ -68,46 +17,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import AppItemListVue from "./components/AppItemList.vue";
+import DomainList from "./components/DomainList.vue";
 
 export default {
   name: "App",
   components: {
-    AppItemListVue,
-  },
-  data: function () {
-    return {
-      prefixes: ["Air", "Jat", "Flight"],
-      sufixes: ["Hub", "Station", "Mart"],
-    };
-  },
-  methods: {
-    addPrefix(prefix) {
-      this.prefixes.push(prefix);
-    },
-    deletePrefix(prefix) {
-      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-    },
-    addSufix(sufix) {
-      this.sufixes.push(sufix);
-    },
-    deleteSufix(sufix) {
-      this.sufixes.splice(this.prefixes.indexOf(sufix), 1);
-    },
-  },
-  computed: {
-    domains() {
-      const domains = [];
-      for (const prefix of this.prefixes) {
-        for (const sufix of this.sufixes) {
-          const name = prefix + sufix;
-          const url = name.toLowerCase();
-          const checkout = `http://www.hostgator.com.br/${url}`;
-          domains.push({ name, checkout });
-        }
-      }
-      return domains;
-    },
+    DomainList,
   },
 };
 </script>
