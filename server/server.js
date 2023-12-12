c5aea5eb610681d4763bf4c5f2c01dd895b59c2c
MiPigurim <michelepigurim26@gmail.com>
2023-12-12 17:18:48 -0300

    feat: test server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,38 @@
 const { ApolloServer } = require("apollo-server");
-console.log("ApolloServer", ApolloServer);
+
+const typeDefs = `
+  typeItem {
+    id: Int
+    type: String
+    description: String
+  }
+  
+  type Query {
+    prefixes: [Item]
+    sufixes: [Item]
+  }
+
+  const prefixes = [
+    { id: 1, type: "prefix", description: "Air" },
+    { id: 2, type: "prefix", description: "Jet" },
+    { id: 3, type: "prefix", description: "Flight" },
+  ];
+
+  const sufixes = [
+    { id: 4, type: "sufix", description: "Hub" },
+    { id: 5, type: "sufix", description: "Station" },
+    { id: 6, type: "sufix", description: "Mart" },
+  ];
+
+  const resolvers = {
+    Query: {
+      prefixes: () {
+        return items.filter((item) => item.type === "prefix");
+      },
+      sifixes: () {
+        return items.filter((item) => item.type === "sufix");
+      }
+  `;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen();
