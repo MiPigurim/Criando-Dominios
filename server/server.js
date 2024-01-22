54e512daba99af3e577c10be5c22bd4dcf0c00fa
MiPigurim <michelepigurim26@gmail.com>
2024-01-22 15:50:55 -0300

    fix(server): query resolvers

--- server/server.js
+++ server/server.js
@@ -1,38 +1,35 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
-  typeItem {
+  type Item {
     id: Int
     type: String
     description: String
   }
   
   type Query {
-    prefixes: [Item]
-    sufixes: [Item]
+    items (type: String): [Item]
   }
-
-  const prefixes = [
-    { id: 1, type: "prefix", description: "Air" },
-    { id: 2, type: "prefix", description: "Jet" },
-    { id: 3, type: "prefix", description: "Flight" },
-  ];
-
-  const sufixes = [
-    { id: 4, type: "sufix", description: "Hub" },
-    { id: 5, type: "sufix", description: "Station" },
-    { id: 6, type: "sufix", description: "Mart" },
-  ];
-
-  const resolvers = {
-    Query: {
-      prefixes: () {
-        return items.filter((item) => item.type === "prefix");
-      },
-      sifixes: () {
-        return items.filter((item) => item.type === "sufix");
-      }
   `;
 
+const items = [
+  { id: 1, type: "prefix", description: "Air" },
+  { id: 2, type: "prefix", description: "Jet" },
+  { id: 3, type: "prefix", description: "Flight" },
+  { id: 4, type: "sufix", description: "Hub" },
+  { id: 5, type: "sufix", description: "Station" },
+  { id: 6, type: "sufix", description: "Mart" },
+];
+
+const resolvers = {
+  Query: {
+    items(_, args) {
+      console.log(args);
+      return items.filter((item) => item.type === args.type);
+    },
+  },
+};
 const server = new ApolloServer({ typeDefs, resolvers });
-server.listen();
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
